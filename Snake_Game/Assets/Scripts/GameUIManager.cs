ca76dc3524e513371fbeb19e1867deee13bd1d62
Hasan Haswary <88339205+hasanhaswary@users.noreply.github.com>
2025-04-29 23:55:44 +0200

    Updated GameUI Functionality

--- Snake_Game/Assets/Scripts/GameUIManager.cs
+++ Snake_Game/Assets/Scripts/GameUIManager.cs
@@ -14,20 +14,38 @@ public class GameUImanger : MonoBehaviour
     public GameObject audioSystem;
     public GameObject unmuteButtonObj;
     public GameObject muteButtonObj;
+    public GameObject gameScreen;
+    public GameObject restartScreen;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //Start State
+        gameScreen.SetActive(true);
+        restartScreen.SetActive(false);
+
+        //Buttons Functionality
         exitButton.onClick.AddListener(GameController.Instance.LoadMenu);
-        restartButton.onClick.AddListener(GameController.Instance.RestartGame);
-        muteButton.onClick.AddListener(() => GameController.Instance.MuteAudio(audioSystem, unmuteButtonObj, muteButtonObj));
-        unmuteButton.onClick.AddListener(() => GameController.Instance.UnmuteAudio(audioSystem, unmuteButtonObj, muteButtonObj));
+        restartButton.onClick.AddListener(GameController.Instance.LoadGame);
+
+        muteButton.onClick.AddListener(() =>
+        {
+            GameController.Instance.MuteAudio(audioSystem, unmuteButtonObj, muteButtonObj);
+        });
+
+        unmuteButton.onClick.AddListener(() =>
+        {
+            GameController.Instance.UnmuteAudio(audioSystem, unmuteButtonObj, muteButtonObj);
+        });
+
+        mainMenuButton.onClick.AddListener(GameController.Instance.LoadMenu);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameEnd()
     {
-        
+        gameScreen.SetActive(true);
+        restartScreen.SetActive(true);
     }
 }
